08f8c440d9d88e1ba5730d54cf0d756edaa30cbb
Mark <mr4ko.bes@yandex.com>
2024-11-02 18:26:49 +0400

    Task 3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 }; 
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayInReverse(array, array.Length - 1);
+    }
+
+    static void PrintArrayInReverse(int[] array, int index)
+    {
+        if (index < 0)
+        {
+            return;
+        }
+        Console.WriteLine(array[index]);
+        PrintArrayInReverse(array, index - 1);
+    }
+}
+
